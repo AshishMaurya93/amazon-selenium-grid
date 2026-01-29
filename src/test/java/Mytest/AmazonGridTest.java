56c4b2fea7d70caeed5bd2b491883e5cdf6aa507
Ashish Maurya <ashishmaurya290@gmail.com>
2026-01-29 23:36:07 +0530

    commit

--- src/test/java/Mytest/AmazonGridTest.java
+++ src/test/java/Mytest/AmazonGridTest.java
@@ -33,9 +33,9 @@ public class AmazonGridTest {
 
     @Test
     public void verifyAmazonTitle() throws InterruptedException {
-    	Thread.sleep(5000);
+    	//Thread.sleep(5000);
         driver.get("https://www.amazon.in");
-        Thread.sleep(3000);
+        //Thread.sleep(3000);
         String title = driver.getTitle();
         System.out.println("Page title is: " + title);
         Assert.assertTrue(title.contains("Amazon"));
