1d9caed280c5d84a7952b1fa5c3f6df0320f93da
Ashish Maurya <ashishmaurya290@gmail.com>
2026-01-29 23:54:29 +0530

    commit

--- src/test/java/Mytest/AmazonGridTest.java
+++ src/test/java/Mytest/AmazonGridTest.java
@@ -33,7 +33,7 @@ public class AmazonGridTest {
 
     @Test
     public void verifyAmazonTitle() throws InterruptedException {
-    	//Thread.sleep(5000);
+    	Thread.sleep(3000);
         driver.get("https://www.amazon.in");
         Thread.sleep(3000);
         String title = driver.getTitle();
